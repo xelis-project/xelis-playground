f02b18ac8b8557f9112aa671d4141a14353510b6
Slixe <slixeprivate@gmail.com>
2024-11-16 22:48:20 +0100

    patch println from env to redirect to console output

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ const logs = document.getElementById('logs');
 const runButton = document.getElementById('runButton');
 
 let code = localStorage.getItem('code');
-if (code === null) {
+if (!code) {
     code = `entry main() {
         println("Hello, World!");
         return 0;
@@ -18,22 +18,29 @@ if (code === null) {
 }
 
 editor.innerText = code;
-hljs.highlightElement(editor);
+// hljs.highlightElement(editor);
 
 runButton.addEventListener('click', () => {
     const code = editor.innerText;
 
     // Placeholder logic for executing Rust code
-    logs.innerText = "Compiling...\n";
+    logs.innerText = "------- Compiling -------\n";
     try {
         let program = silex.compile(code);
         logs.innerText += "Compiled successfully!\n";
 
         localStorage.setItem('code', code);
 
-        logs.innerText += "Running...\n";
-        let exit = silex.execute_program(program, 0);
-        logs.innerText += `Exit code: ${exit}\n`;
+        logs.innerText += "-------- Running --------\n";
+        let result = silex.execute_program(program, 0);
+        let output = result.logs();
+        if (output.length > 0) {
+            logs.innerText += `Output:\n`;
+            logs.innerText += output.join("\n");
+            logs.innerText += "\n";
+        }
+
+        logs.innerText += `Exit code: ${result.value()}\n`;
     } catch (e) {
         logs.innerText += "Error: " + e + "\n";
         return;
--- xelis-playground/src/lib.rs
+++ xelis-playground/src/lib.rs
@@ -1,3 +1,5 @@
+use std::sync::mpsc;
+
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 use xelis_builder::EnvironmentBuilder;
 use xelis_bytecode::Module;
@@ -5,10 +7,12 @@ use xelis_compiler::Compiler;
 use xelis_lexer::Lexer;
 use xelis_parser::Parser;
 use xelis_vm::VM;
+use xelis_types::Type;
 
 #[wasm_bindgen]
 pub struct Silex {
     environment: EnvironmentBuilder<'static>,
+    logs_receiver: mpsc::Receiver<String>,
 }
 
 #[wasm_bindgen]
@@ -17,11 +21,47 @@ pub struct Program {
 }
 
 #[wasm_bindgen]
+pub struct ExecutionResult {
+    value: String,
+    logs: Vec<String>,
+}
+
+#[wasm_bindgen]
+impl ExecutionResult {
+    pub fn value(&self) -> String {
+        self.value.clone()
+    }
+
+    pub fn logs(&self) -> Vec<String> {
+        self.logs.clone()
+    }
+}
+
+static mut LOGS_SENDER: Option<mpsc::Sender<String>> = None;
+
+#[wasm_bindgen]
 impl Silex {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
+        let mut environment = EnvironmentBuilder::default();
+        // Patch the environment to include a println function that sends logs to the receiver
+        let (sender, receiver) = mpsc::channel();
+
+        unsafe {
+            LOGS_SENDER = Some(sender);
+        }
+
+        environment.get_mut_function("println", None, vec![Type::Any])
+            .set_on_call(move |_, args| -> _ {
+                let param = &args[0];
+                let sender = unsafe { LOGS_SENDER.as_ref().unwrap() };
+                sender.send(format!("{}", param.as_ref().as_value())).unwrap();
+                Ok(None)
+            });
+
         Self {
-            environment: EnvironmentBuilder::default(),
+            environment,
+            logs_receiver: receiver,
         }
     }
 
@@ -46,13 +86,18 @@ impl Silex {
         }
     }
 
-    pub fn execute_program(&self, program: Program, chunk_id: u16) -> Result<String, JsValue> {
+    pub fn execute_program(&self, program: Program, chunk_id: u16) -> Result<ExecutionResult, JsValue> {
         let mut vm = VM::new(&program.module, self.environment.environment());
         vm.invoke_entry_chunk(chunk_id)
             .map_err(|err| JsValue::from_str(&format!("{:#}", err)))?;
 
-        match vm.run() {
-            Ok(value) => Ok(format!("{}", value)),
+        let res = vm.run();
+        let logs = self.logs_receiver.try_iter().collect();
+        match res {
+            Ok(value) => Ok(ExecutionResult {
+                value: format!("{}", value),
+                logs,
+            }),
             Err(err) => Err(JsValue::from_str(&format!("{:#}", err))),
         }
     }
