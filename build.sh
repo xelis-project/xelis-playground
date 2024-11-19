79d1bc30e7f435f016a86d423421b2c72bdde387
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-11-19 13:52:56 -0500

    make build install wasm-pack

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+# Make sure Cargo is installed and needed as a dependency to create the WASM lib
+if command -v cargo &> /dev/null; then
+  echo "Cargo is installed."
+else 
+  echo "Installing Cargo..."
+
+  # Download & run rustup installer
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable -y
+
+  # Source the Cargo env
+  source "$HOME/.cargo/env"
+
+  echo "Cargo installed successfully."
+fi
+
+# Make sure wasm-pack is installed
+cargo install wasm-pack
+
+# Create WASM lib for javascript browser
 export RUSTUP_TOOLCHAIN="nightly"
 export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals"
 wasm-pack build xelis-playground --target web --out-dir ../public --no-typescript --no-package -- -Z build-std=std,panic_abort
