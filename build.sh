17409858a54c68ce620e503f94f370472307bda5
Slixe <slixeprivate@gmail.com>
2025-01-03 22:51:11 +0100

    update dependencies

--- build.sh
+++ build.sh
@@ -24,4 +24,4 @@ cargo install wasm-pack
 
 export RUSTUP_TOOLCHAIN="nightly"
 export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals"
-wasm-pack build xelis-playground --target web --out-dir ../public --no-typescript --no-package -- -Z build-std=std,panic_abort
+wasm-pack build --release xelis-playground --target web --out-dir ../public --no-typescript --no-package -- -Z build-std=std,panic_abort
