ab495b162f1c60191cde19d87063b69b36255b65
codehalo <randi@codehalo.com>
2025-09-03 16:22:12 -0400

    Begin adding delete export to files.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import modify from 'rollup-plugin-modify';
 import { defineConfig } from 'vite';
+import magicalSvg from "vite-plugin-magical-svg";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ command }) => {
@@ -11,6 +12,7 @@ export default defineConfig(({ command }) => {
             }
         },
         build: {
+            assetsInlineLimit: Number.MAX_SAFE_INTEGER,
             rollupOptions: {
                 external: ['../public/xelis_playground.js', '/xelis_playground.js'], // this has to be external (untouched) - can't run silex if it's bundled / in production
                 output: {
@@ -24,7 +26,35 @@ export default defineConfig(({ command }) => {
                 }
             }
         },
-        plugins: []
+        plugins: [
+            magicalSvg({
+                // By default, the output will be a dom element (the <svg> you can use inside the webpage).
+                // You can also change the output to react (or any supported target) to get a component you can use.
+                target: 'dom',
+
+                // By default, the svgs are optimized with svgo. You can disable this by setting this to false.
+                svgo: false,
+
+                // By default, width and height set on SVGs are not preserved.
+                // Set to true to preserve `width` and `height` on the generated SVG.
+                preserveWidthHeight: false,
+
+                // *Experimental* - set the width and height on generated SVGs.
+                // If used with `preserveWidthHeight`, will only apply to SVGs without a width/height.
+                //setWidthHeight: { width: '32', height: '32' },
+
+                // *Experimental* - replace all instances of `fill="..."` and `stroke="..."`.
+                // Set to `true` for 'currentColor`, or use a text value to set it to this value.
+                // When enabled, use query param ?skip-recolor to not alter colors.
+                // Disabled by default.
+                setFillStrokeColor: true,
+
+                // *Experimental* - if a SVG comes with `width` and `height` set but no `viewBox`,
+                // assume the viewbox is `0 0 {width} {height}` and add it to the SVG.
+                // Disabled by default.
+                restoreMissingViewBox: true,
+            }),
+        ],
     }
 
     if (command === `build`) {
